77ae093d8475de39a431c96c29c395c1fbbf4e66
ciaran mccamphill <ciaran.mccamphill@yahoo.com>
2023-08-06 23:58:12 +0100

    further testing

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ public class WeatherForecastController : ControllerBase
 
         _logger.LogError("This is an ERROR test");
 
-        _logger.LogInformation("This is to test drone update");
+        _logger.LogInformation("This is a drone test");
         
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
