1704ecb24df3613d6c7a3da11335b3d7e6de7d71
ciaran mccamphill <ciaran.mccamphill@yahoo.com>
2023-12-22 00:01:11 +0000

    test4

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Derry", "Strabane", "Coleraine","Belfast", "Test", "Limavady"
+        "Derry", "Strabane", "Coleraine","Belfast"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
